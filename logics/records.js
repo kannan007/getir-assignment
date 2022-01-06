328df8f168a130dca8ddf7beed5f41c550216802
Kannan <kannant92@yahoo.in>
2022-01-06 23:09:09 +0530

    comments added

--- logics/records.js
+++ logics/records.js
@@ -19,6 +19,12 @@ const getRecords = async (bodyData) => {
   } = bodyData;
 
   let paginationPipeline = [];
+
+  /* 
+    As there are 4k records available in that collection its always better to use pagination however
+    after checking with panel don't change the input data so i wrote it as optional if we get page and count from
+    front end team will do pagination accordingly 
+   */
   if (page && count) {
     const skip = (+page ? +page - 1 : +page) * +count;
     const limit = +count;
@@ -32,15 +38,18 @@ const getRecords = async (bodyData) => {
     );
   }
 
+  // Input Valid Data check
   if (!startDate || !endDate || !minCount || !maxCount)
     throw new Error(`Input data is missing`);
 
+  // Valid date format check
   if (!isValidDate(new Date(startDate)) || !isValidDate(new Date(endDate)))
     throw new Error(`Please send valid date format`);
 
   minCount = +minCount;
   maxCount = +maxCount;
 
+  // Valid Number check for min/max count
   if (!Number.isInteger(minCount) || !Number.isInteger(maxCount))
     throw new Error(`Please send mincount or maxcount in integer format`);
 
@@ -70,7 +79,7 @@ const getRecords = async (bodyData) => {
       {
         $sort: { totalCount: 1 },
       },
-      ...paginationPipeline
+      ...paginationPipeline,
     ])
     .catch((err) => {
       throw new Error(`Error fetching Records`);
